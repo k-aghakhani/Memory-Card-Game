411760342d6fdea0498ab348f7e2ad26c75f766b
Kiarash Aghakhani <kiarash88ca@gmail.com>
2025-01-30 18:35:59 -0800

    Main Class (MainActivity.java) This class manages the game.

--- app/src/main/java/com/aghakhani/memorycardgame/MainActivity.java
+++ app/src/main/java/com/aghakhani/memorycardgame/MainActivity.java
@@ -7,18 +7,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private TextView tvScore;
+    private int score = 0;
+    private List<MemoryCard> cardList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        tvScore = findViewById(R.id.tvScore);
+
+        int[] images = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5, R.drawable.img6};
+        cardList = new ArrayList<>();
+        for (int img : images) {
+            cardList.add(new MemoryCard(img));
+            cardList.add(new MemoryCard(img));
+        }
+        Collections.shuffle(cardList);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 4));
+        recyclerView.setAdapter(new MemoryAdapter(this, cardList, () -> {
+            score += 10;
+            tvScore.setText("امتیاز: " + score);
+            if (score == images.length * 10) {
+                Toast.makeText(this, "تبریک! شما برنده شدید!", Toast.LENGTH_LONG).show();
+            }
+        }));
     }
 }
